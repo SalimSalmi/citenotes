ddb1ef51a345d51366cda7098e59564aa9a6c001
Salim Salmi <salmi0707@gmail.com>
2023-06-28 09:16:02 +0200

    Changed request url to accept arxiv

--- src/http.rs
+++ src/http.rs
@@ -1,15 +1,32 @@
 use reqwest::header::{HeaderMap, HeaderValue, ACCEPT};  
+use citenotes::CiteNoteError;
+use std::error::Error;
 
-pub fn request_bib_with_doi(url: &String) -> Result<String, reqwest::Error> {
+fn request(url: &String) -> Result<reqwest::blocking::Response, reqwest::Error> {
     let mut headers = HeaderMap::new();
     headers.insert(
         ACCEPT,
-        HeaderValue::from_static("text/bibliography; style=bibtex"),
+        HeaderValue::from_static("application/x-bibtex; charset=utf-8"),
     );
 
     let client = reqwest::blocking::Client::new();
-    let response = client.get(url).headers(headers).send()?;
-    let body = response.text()?;
+    client.get(url).headers(headers).send()
+}
 
+fn response_error(response: &reqwest::blocking::Response) -> Result<&reqwest::blocking::Response, CiteNoteError> {
+    if response.status().is_success() {
+        Ok(response)
+    } else {
+        let status = response.status();
+        let message = format!("Request failed with error response: {}", status);
+        Err(CiteNoteError::new(message.as_str()))
+    } 
+}
+
+pub fn request_bib_with_doi(url: &String) -> Result<String, Box<dyn Error>> {
+    let response = request(url)?;
+    response_error(&response)?;
+
+    let body = response.text()?;
     Ok(body)
 }
